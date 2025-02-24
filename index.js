d75a02a2ecb6f529b9ff9c9e2169b2958f202dcf
aved2 <ved3@illinois.edu>
2025-02-23 22:41:03 -0600

    Updates

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ import { fileURLToPath } from "url";
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = dirname(__filename);
 var vite_config_default = defineConfig({
+  base: "/ScheduleMasterLandingPage/",
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
